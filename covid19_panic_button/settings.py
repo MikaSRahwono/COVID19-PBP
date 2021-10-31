a66f771f455ab25055859463f57cedf35fb0a7ba
Mika Suryofakhri Rahwono <mikasuryof@outlook.com>
2021-10-31 20:06:03 +0700

    Preparing deployment to heroku

--- covid19_panic_button/settings.py
+++ covid19_panic_button/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.cont,rib.sessions',
+    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
